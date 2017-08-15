43a89449b86fcc6791c2705e2a3618b12e70e31d
Trevor McGarrah <tdmcg82@yahoo.com>
2017-08-14 20:30:25 -0500

    Added random emoji with http server

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const emoji = require('node-emoji');
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.write(emoji.random().emoji);
+  res.end();
+});
+
+server.listen(8001);
+
 let emojiArray = [];
 let num = 12;
 
 for (var i = 0; i < num; i++) {
-  emojiArray.push(emoji.random());
+  emojiArray.push(emoji.random().emoji);
 }
 
-function loopEmojis (emojiArray) {
-  for (var i = 0; i < emojiArray.length; i++) {
-    console.log(emojiArray[i].emoji);
-  }
-};
+// function loopEmojis (emojiArray) {
+//   for (var i = 0; i < emojiArray.length; i++) {
+//     console.log(emojiArray[i].emoji);
+//   }
+// };
 
-loopEmojis(emojiArray);
+//loopEmojis(emojiArray);
